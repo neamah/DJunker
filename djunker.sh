f8b5a72aea4b14dd80e5399171a2b952f033586e
Neamah Hussein <neamahhs@gmail.com>
2015-08-22 23:03:55 -0700

    Add user input code

--- djunker.sh
+++ djunker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Hello $USER 
+echo Hello $USER
 
 # The target file
 FILE=
@@ -10,10 +10,10 @@ RAND=$(gshuf -i0-1 -n1)
 # Randomly: Either give an old file, or give a random file
 
 if [ $RAND = 0 ]; then
-    echo "Here's a random file. Do you need it?"
+    echo "Here's a RANDOM file. Do you want to delete it?"
     FILE=$(ls ~/Downloads | gshuf -n 1)
-else 
-    echo "Here's a really old file! Do you still need it?"
+else
+    echo "Here's a really OLD file! Do you want to delete it?"
     cd ../../Downloads
     FILE=$(ls -tr1 | head -1)
     cd ../Projects/DJunker
@@ -21,7 +21,8 @@ fi
 
 echo $FILE
 
-
- 
+select ANS in "Yes" "No" "Later"; do
+    case $ANS in
+        Yes )
 
 
